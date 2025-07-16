3de4f04ec227f40663dd5a7898f296ecb215e164
unknown <rdauphinelys@gmail.com>
2025-07-16 23:46:05 +0300

    f

--- config/geoip.php
+++ config/geoip.php
@@ -55,6 +55,17 @@ return [
 
     'services' => [
 
+        'maxmind' => [
+    'class' => \Torann\GeoIP\Services\MaxMindDatabase::class,
+    'database_path' => storage_path('app/geoip.mmdb'),
+    'update_url' => sprintf(
+        'https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=%s&suffix=tar.gz',
+        env('MAXMIND_LICENSE_KEY')
+    ),
+    'locales' => ['en'],
+],
+
+
         'maxmind_database' => [
             'class' => \Torann\GeoIP\Services\MaxMindDatabase::class,
             'database_path' => storage_path('app/geoip.mmdb'),
