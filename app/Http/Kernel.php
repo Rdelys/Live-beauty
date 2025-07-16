dd1671551e53793c2b8621fe9868e6413233bea9
unknown <rdauphinelys@gmail.com>
2025-07-16 23:11:01 +0300

    error correction

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,7 +21,6 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-            \App\Http\Middleware\BlockCountries::class,
 
     ];
 
@@ -38,13 +37,16 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+                \App\Http\Middleware\BlockCountries::class,
+
         ],
 
         'api' => [
-            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        ],
+    // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+    'throttle:api',
+    \Illuminate\Routing\Middleware\SubstituteBindings::class,
+],
+
     ];
 
     /**
@@ -69,9 +71,8 @@ class Kernel extends HttpKernel
         'auth.modele' => \App\Http\Middleware\AuthModele::class,
     ];
 
-    protected $routeMiddleware = [
-    // autres...
+   protected $routeMiddleware = [
+    // autres middlewares
     'block.countries' => \App\Http\Middleware\BlockCountries::class,
 ];
-
 }
