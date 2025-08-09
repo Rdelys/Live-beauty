691d843786a4c7050cefe92f5b56d4ce1d4bd89d
unknown <rdauphinelys@gmail.com>
2025-08-09 23:02:12 +0300

    url

--- resources/views/modele/profil.blade.php
+++ resources/views/modele/profil.blade.php
@@ -504,7 +504,7 @@ let stream;
 const peerConnections = {};
 
 /* === CONNEXION SOCKET.IO (unique) === */
-socket = io("http://localhost:3000", {
+socket = io("http://livebeautyofficial.com/", {
     path: '/socket.io',
     transports: ['websocket']
 });
