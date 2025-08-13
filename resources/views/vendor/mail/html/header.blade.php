0a599b0c768732ac4ce0319c249e14db185e451a
unknown <rdauphinelys@gmail.com>
2025-08-13 23:00:19 +0300

    mail

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,13 @@
+@props(['url'])
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+@if (trim($slot) === 'Laravel')
+<!--<img src="{{ asset('images/logo.png') }}" class="logo" alt="LiveBeautyOfficielle Logo">-->
+<p>LIVE BEAUTY OFFICIELLE</p>
+@else
+{{ $slot }}
+@endif
+</a>
+</td>
+</tr>
