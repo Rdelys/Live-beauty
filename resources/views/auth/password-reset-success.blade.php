6baae53ede50361f928268a5fbe39ab944f21cb7
unknown <rdauphinelys@gmail.com>
2025-12-04 20:30:52 +0300

    correction erreur sur recup mdp

--- resources/views/auth/password-reset-success.blade.php
+++ resources/views/auth/password-reset-success.blade.php
@@ -7,9 +7,12 @@
       <div class="alert alert-success text-center shadow-lg p-5 rounded bg-dark text-white">
         <h4 class="mb-3">✅ {{ __('Mot de passe modifié avec succès ') }}
 !</h4>
-        <p>{{ __('Vous allez être redirigé vers la page d'accueil dans') }}
- <span id="countdown">5</span>{{ __('secondes') }}
- .</p>
+       <p>
+  {{ __("Vous allez être redirigé vers la page d'accueil dans") }}
+  <span id="countdown">5</span>
+  {{ __("secondes") }}.
+</p>
+
         <p class="small text-muted">{{ __('Si la redirection ne fonctionne pas') }}
 , <a href="{{ url('/') }}" class="text-warning">{{ __('cliquez ici') }}
 </a>.</p>
