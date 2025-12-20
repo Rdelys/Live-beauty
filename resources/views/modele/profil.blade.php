153e78e9fbfc2099cc67ce567e26b0cda9b51d86
Rdelys <rdauphinelys@gmail.com>
2025-12-20 19:19:31 +0300

    correction jetons pas marché

--- resources/views/modele/profil.blade.php
+++ resources/views/modele/profil.blade.php
@@ -1541,7 +1541,7 @@ let stream;
 const peerConnections = {};
 
 /* === CONNEXION SOCKET.IO (unique) === */
-socket = io("http://localhost:3000/", {
+socket = io("wss://livebeautyofficial.com", {
     path: '/socket.io',
     transports: ['websocket']
 });
